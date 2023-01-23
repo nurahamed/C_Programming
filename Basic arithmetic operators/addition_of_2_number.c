94bab564415e24bb2b3fdf9628717c6f64adfc01
Nur Ahamed Mondal <nurahamed62@gmail.com>
2023-01-24 00:37:18 +0530

    addition of two number

---
+++ Basic arithmetic operators/addition_of_2_number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int a, b;
+  printf("Enter a:");
+  scanf("%d", &a);
+  printf("Enter b:");
+  scanf("%d", &b);
+  // I have add the two number in the printf statement
+  printf("Addition is: %d" a+b);
+
+  return 0;
+}
